c82dc3bddff2dd55b763d7c737dbc7dba69d77d1
Nikos Filippakis <aesmade@gmail.com>
2018-06-22 01:22:17 +0300

    Move everything to components
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- src/app.js
+++ src/app.js
@@ -1,39 +1,78 @@
 const appName = 'Indecision app';
-let opts = []
-const doSubmit = e => {
-    e.preventDefault()
-    const opt = e.target.elements.option.value
-    console.log(`submit: ${opt}`)
-    if (opt) {
-        opts.push(opt)
-        e.target.elements.option.value = ''
+let optstore = []
+
+class AddForm extends React.Component {
+    doSubmit(e) {
+        e.preventDefault()
+        const opt = e.target.elements.option.value
+        console.log(`submit: ${opt}`)
+        if (opt) {
+            this.props.opts.push(opt)
+            e.target.elements.option.value = ''
+            rend()
+        }
+    }
+
+    removeAll() {
+        let opts = this.props.opts
+        opts.splice(0, opts.length)
         rend()
     }
+
+    render() {
+        let opts = this.props.opts
+        return (
+            <div>
+                <form onSubmit={ (e) => this.doSubmit(e) }>
+                    <input type="text" name="option" />
+                    <button>Add</button>
+                </form>
+                <button onClick={ () => this.removeAll() }>Remove all</button>
+            </div>
+        )
+    }
 }
-const removeAll = () => {
-    opts = []
-    rend()
+
+class OptionsView extends React.Component {
+    render() {
+        let opts = this.props.opts
+        return (
+            <div>
+                { opts.length > 0 ? <p>You have these options:</p> : <p>No options</p> }
+                <ol>
+                    { opts.map((x, i) => <li key={i}>{x}</li>) }
+                </ol>
+            </div>
+        )
+    }
 }
-const makeSel = () => {
-    const choice = Math.floor(Math.random() * opts.length)
-    alert(opts[choice])
+
+class IndecisionApp extends React.Component {
+    constructor(props) {
+        super(...arguments)
+    }
+
+    makeSel() {
+        let opts = this.props.opts
+        const choice = Math.floor(Math.random() * opts.length)
+        alert(opts[choice])
+    }
+
+    render() {
+        let opts = this.props.opts
+        return (
+            <div>
+                <h1>{appName}</h1>
+                <OptionsView opts={opts} />
+                <AddForm opts={opts} />
+                <button disabled={opts.length == 0} onClick={ () => this.makeSel() }>Select random</button>
+            </div>
+        )
+    }
 }
 
 const rend = () => {
-let templ = <div>
-    <h1>{appName}</h1>
-    { opts.length > 0 ? <p>You have these options:</p> : <p>No options</p> }
-    <ol>
-        { opts.map((x, i) => <li key={i}>{x}</li>) }
-    </ol>
-    <form onSubmit={ doSubmit }>
-        <input type="text" name="option" />
-        <button>Add</button>
-    </form>
-    <button onClick={removeAll}>Remove all</button>
-    <button disabled={opts.length == 0} onClick={makeSel}>Select random</button>
-    </div>
-
-ReactDOM.render(templ, document.getElementById('app'))
+    let templ = <IndecisionApp opts={optstore} />
+    ReactDOM.render(templ, document.getElementById('app'))
 }
 rend()
