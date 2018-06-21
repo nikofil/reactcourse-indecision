ef726c7e6beaf8276b786ffc7652200e7d5bd395
Nikos Filippakis <aesmade@gmail.com>
2018-06-21 23:52:00 +0300

    Basic rerendering
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- public/scripts/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var templ = React.createElement(
-  'p',
-  null,
-  'Hello world'
-);
-
-ReactDOM.render(templ, document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
-let templ = <p>Hello world</p>
+const appName = 'Indecision app';
+let opts = ['a','b']
+let count = 0
+const add = () => {
+    console.log('add')
+    count++
+    rend()
+}
+const sub = () => {
+    console.log('sub')
+    count--
+    rend()
+}
+const reset = () => {
+    console.log('res')
+    count = 0
+    rend()
+}
+let rend = () => {
+let templ = <div>
+    <h1>{appName}</h1>
+    <p>Count: {count}</p>
+    <button onClick={add} className="button">add</button>
+    <button onClick={sub} className="button">sub</button>
+    <button onClick={reset} className="button">reset</button>
+    </div>
 
 ReactDOM.render(templ, document.getElementById('app'))
+}
+rend()
