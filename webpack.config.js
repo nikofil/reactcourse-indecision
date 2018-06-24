4103f790740e73b1a8af4bec4ea1581c878913c9
Nikos Filippakis <aesmade@gmail.com>
2018-06-24 16:18:28 +0300

    Webpack devServer
    Signed-off-by: Nikos Filippakis <aesmade@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path')
 
+const public = path.join(__dirname, 'public/')
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public/'),
+        path: public,
         filename: 'bundle.js'
     },
     module: {
@@ -12,6 +13,9 @@ module.exports = {
             test: /\.js$/,
             exclude: /node-modules/
         }]
+    },
+    devServer: {
+        contentBase: public
     },
     devtool: 'cheap-module-eval-source-map'
 }
